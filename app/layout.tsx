f4ece14395f87b69aafae10f6a7cb87e21e3ebfc
kash2104 <kparikh1104@gmail.com>
2024-12-28 18:39:04 +0530

    added: wrapped the app in context provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./provider";
+import { Providers } from "@/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
+        <Providers> 
           {children}
         </Providers>
       </body>
