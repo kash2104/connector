d1f8caf1f32950c55f36be2721417c8657dc8d41
kash2104 <kparikh1104@gmail.com>
2024-12-30 00:47:05 +0530

    added: loading and error page

---
+++ app/components/Error/page.tsx
@@ -0,0 +1,12 @@
+export default function Error() {
+    return (
+      <div className="bg-[#0F172A] flex items-center justify-center px-4">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-[#38BDF8] mb-2">404</h1>
+          <p className="text-xl text-white">Page Not Found</p>
+        </div>
+      </div>
+    )
+  }
+  
+  
---
+++ app/components/Loading/page.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+    return (
+      <div className="bg-[#0F172A] flex items-center justify-center px-4">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-[#38BDF8] mb-2">Loading...</h1>
+          <p className="text-xl text-white">Please wait</p>
+        </div>
+      </div>
+    )
+  }
+  
+  
