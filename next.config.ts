ab567015fd8e1d74d782a1e872cb64bbfc8d104b
kash2104 <kparikh1104@gmail.com>
2024-12-31 18:37:10 +0530

    update: added cloudinary path for next/image in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: `/${process.env.CLOUD_NAME}/**`,
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
